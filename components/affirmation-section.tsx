5cdfd2df0a17e1e7e418e30543cffa27599debe8
NidhiD26 <dineshnidhi2605@gmail.com>
2025-05-10 22:17:26 +0530

    affirmation

--- components/affirmation-section.tsx
+++ components/affirmation-section.tsx
@@ -20,9 +20,8 @@ export default function AffirmationSection() {
     "Healing happens one moment at a time. Celebrate each small victory.",
     "You have overcome difficult days before, and you will overcome this too.",
     "Your journey matters. Your story matters. You matter.",
-    "I can and i will overcome this"
-    "No matter how hard it gets, I will keep fighting"
-
+    "I can and i will overcome this",
+    "No matter how hard it gets, I will keep fighting",
   ]
 
   useEffect(() => {
